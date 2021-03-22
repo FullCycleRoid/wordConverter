c4d1fd7212634a7dbe497800ccb436a3970ddcbf
FullCircleRoid <andrewthechf@gmail.com>
2021-03-22 23:25:13 +0300

    init:converter

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import re
 import mammoth
 from atom_weight import atom_weight
+from bs4 import BeautifulSoup
 
 forbidden_words = ['Макс.', 'Среднее', 'Станд. отклонение', 'стат.', 'отклонение', 'Станд.', 'Мин.']
 forbidden_values = [ 'Да', 'Итог', '100.00', 'В стат.']
@@ -26,8 +27,19 @@ def clean_tags(row_html):
     return re.split('<.*?>', row_html)
 
 
-def split_table_rows(table):
-    return re.findall(r'<tr>((.|\n)*?)<\/tr>', table)
+def add_zero_value(row):
+    # print(row)
+    new_row = ""
+    soup = BeautifulSoup(row, 'html.parser')
+    tags = soup.find_all("td")
+    for tag in tags:
+        tag = str(tag)
+        if tag == "<td></td>":
+            tag = "<td>0</td>"
+            new_row += tag
+        else:
+            new_row += tag
+    return new_row
 
 
 def clean_row(row):
@@ -46,6 +58,12 @@ def remove_spaces(row):
     return new_row
 
 
+def remove_emtpy_row(row):
+    if row[0] == '0':
+        return
+    return row
+
+
 def remove_forbidden_values(row):
     new_row = []
     for item in row:
@@ -56,34 +74,64 @@ def remove_forbidden_values(row):
 
 html_tuple = extract_tables(html)
 
-
 for table in html_tuple:
     tables.append([])
     for row in extract_rows(table[0]):
-        cleaned_row = clean_tags(row[0])
+        string_without_spaces = add_zero_value(row[0])
+        cleaned_row = clean_tags(string_without_spaces)
         cleaned_row = remove_spaces(cleaned_row)
-        if cleaned_row != [] and cleaned_row[0] not in forbidden_words:
+        cleaned_row = remove_emtpy_row(cleaned_row)
+        if cleaned_row != None and cleaned_row[0] not in forbidden_words:
             cleaned_row = remove_forbidden_values(cleaned_row)
             tables[-1].append(cleaned_row)
 
-
-def zip_by_element(table):
-    return [a for a in zip(*table)]
+# def zip_by_element(table):
+#     return [a for a in zip(*table)]
 
 
 spectrus = []
-
-for table in tables:
-    spectrus.append(zip_by_element(table))
-
 atomic_weight = []
 
 
-def convert_to_atomic(spectre):
-    print(spectre)
+for table in tables:
+    new_table = []
+    new_table.append(table[0])
+    table_length = len(table)
+
+    for cycle in range(table_length):
+        new_table.append([])
+
+    current_position = 1
+    current_table = 1
+
+    for element in table[0][1:]:
+        for atomic in atom_weight:
+            if element == atomic[0]:
+                while current_position < table_length:
+                    new_table[current_position].append(float(table[current_table][current_position])/atomic[1])
+                    current_position += 1
+
+    print(new_table)
+
+#
+# def convert_to_atomic(spectre):
+#     title = spectre[0]
+#     atomic_weight[-1].append(title)
+#     temp = []
+#     for row in spectre[1:]:
+#         # print(row)
+#         for item in atom_weight:
+#             if row[0] == item[0]:
+#                 temp.append(item[0])
+#                 for element in row[1:]:
+#                     temp.append(float(element))
+#         atomic_weight[-1].append(tuple(temp))
+#         temp = []
+#
+#
+# for spectre in spectrus:
+#     atomic_weight.append([])
+#     convert_to_atomic(spectre)
 
 
-for spectre in spectrus:
-    convert_to_atomic(spectre)
-
-
+# print(atomic_weight)
