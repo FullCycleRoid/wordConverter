8ac69238f16997fd43e44fec0f0726de8e78c238
FullCycleRoid <andrewthechf@gmail.com>
2021-03-23 17:28:22 +0300

    feat: table calculation

--- converter.py
+++ converter.py
@@ -3,12 +3,13 @@ import mammoth
 from atom_weight import atom_weight
 from bs4 import BeautifulSoup
 
+
 forbidden_words = ['Макс.', 'Среднее', 'Станд. отклонение', 'стат.', 'отклонение', 'Станд.', 'Мин.']
 forbidden_values = [ 'Да', 'Итог', '100.00', 'В стат.']
 tables = []
 converted_values = []
 
-with open("174.docx", "rb") as docx_file:
+with open("138.docx", "rb") as docx_file:
     result = mammoth.convert_to_html(docx_file)
     html = result.value # The generated HTML
     messages = result.messages # Any messages, such as warnings during conversion
@@ -28,7 +29,6 @@ def clean_tags(row_html):
 
 
 def add_zero_value(row):
-    # print(row)
     new_row = ""
     soup = BeautifulSoup(row, 'html.parser')
     tags = soup.find_all("td")
@@ -74,6 +74,7 @@ def remove_forbidden_values(row):
 
 html_tuple = extract_tables(html)
 
+
 for table in html_tuple:
     tables.append([])
     for row in extract_rows(table[0]):
@@ -81,57 +82,73 @@ for table in html_tuple:
         cleaned_row = clean_tags(string_without_spaces)
         cleaned_row = remove_spaces(cleaned_row)
         cleaned_row = remove_emtpy_row(cleaned_row)
+        # print("cleaned_row", cleaned_row)
         if cleaned_row != None and cleaned_row[0] not in forbidden_words:
             cleaned_row = remove_forbidden_values(cleaned_row)
             tables[-1].append(cleaned_row)
 
-# def zip_by_element(table):
-#     return [a for a in zip(*table)]
-
 
 spectrus = []
 atomic_weight = []
-
+temp_table = []
 
 for table in tables:
     new_table = []
-    new_table.append(table[0])
+    header = table[0]
+    new_table.append(header)
     table_length = len(table)
 
-    for cycle in range(table_length):
-        new_table.append([])
-
     current_position = 1
-    current_table = 1
+    current_row = 1
+
+    for cycle in range(table_length-1):
+        new_table.append([])
+        new_table[current_row].append(table[current_row][0])
+        current_row += 1
+    current_row = 1
 
     for element in table[0][1:]:
         for atomic in atom_weight:
             if element == atomic[0]:
-                while current_position < table_length:
-                    new_table[current_position].append(float(table[current_table][current_position])/atomic[1])
-                    current_position += 1
+                while current_row < table_length:
+                    # if element == "O":
+                        # print("Oxigen", table[current_row][0], table[current_row][current_position], atomic[1])
+                        # print("value", (float(table[current_row][current_position]) / atomic[1]))
+                    new_table[current_row].append(float(table[current_row][current_position])/atomic[1])
+                    current_row += 1
+        current_position += 1
+        current_row = 1
+    temp_table.append(new_table)
 
-    print(new_table)
+final_table = []
 
-#
-# def convert_to_atomic(spectre):
-#     title = spectre[0]
-#     atomic_weight[-1].append(title)
-#     temp = []
-#     for row in spectre[1:]:
-#         # print(row)
-#         for item in atom_weight:
-#             if row[0] == item[0]:
-#                 temp.append(item[0])
-#                 for element in row[1:]:
-#                     temp.append(float(element))
-#         atomic_weight[-1].append(tuple(temp))
-#         temp = []
-#
-#
-# for spectre in spectrus:
-#     atomic_weight.append([])
-#     convert_to_atomic(spectre)
+for table in temp_table:
+    new_table = []
+    row_sum = None
+    header = table[0]
+    new_table.append(header)
+    table_length = len(table)
 
+    current_position = 1
+    current_row = 1
+    for cycle in range(table_length-1):
+        new_table.append([])
+        new_table[current_row].append(table[current_row][0])
 
-# print(atomic_weight)
+        # print(table[current_row][0], row_sum)
+        current_row += 1
+    current_row = 1
+    for element in table[0][1:]:
+        while current_row < table_length:
+            row_value = [float(item) for item in table[current_row][1:]]
+            row_sum = sum(row_value)
+            # print(table[current_row][0], "position", current_row, current_position, table[current_row][current_position])
+            value = round(float(table[current_row][current_position]) / row_sum * 100, 2)
+            # print(row_sum)
+            new_table[current_row].append(value)
+            current_row += 1
+        current_position += 1
+
+        current_row = 1
+    final_table.append(new_table)
+print(final_table)
